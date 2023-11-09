7754d95ee4cb3bac9ed6f94508ee08246ec4e9fd
Alcybr <aslahaddin@gmail.com>
2023-11-08 22:03:17 -0600

    evaluator: regex success-signals + per-attack scoring matrix

--- src/evaluator.py
+++ src/evaluator.py
@@ -9,3 +9,17 @@ def evaluate(response: str, success_signal: str) -> bool:
     return bool(re.search(success_signal, response, flags=re.IGNORECASE))
 
 
+def summarize(results):
+    total = len(results)
+    succeeded = sum(1 for r in results if r['succeeded'])
+    by_cat = {}
+    for r in results:
+        c = r['category']
+        by_cat.setdefault(c, {'total': 0, 'succeeded': 0})
+        by_cat[c]['total'] += 1
+        by_cat[c]['succeeded'] += int(r['succeeded'])
+
+    print(f'\n=== Summary ===')
+    print(f'overall: {succeeded}/{total} attacks succeeded ({succeeded / total * 100:.0f}%)\n')
+    for cat, s in sorted(by_cat.items()):
+        print(f'  {cat:30s}  {s["succeeded"]}/{s["total"]}')
